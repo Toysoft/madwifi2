ce3bdf5c74b3e58ae819cd72881b44aa35ae136c
samleffler <samleffler@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2004-01-03 18:48:25 +0000

    add PCI device id for 3COM 3CRDAG675
    Submitted by:	Evgeni Gechev

--- cvs-import/trunk/driver/if_ath_pci.c
+++ cvs-import/trunk/driver/if_ath_pci.c
@@ -78,6 +78,7 @@ static struct pci_device_id ath_pci_id_table[] __devinitdata = {
 	{ 0x168c, 0x0012, PCI_ANY_ID, PCI_ANY_ID },
 	{ 0x168c, 0x0013, PCI_ANY_ID, PCI_ANY_ID },
 	{ 0xa727, 0x0013, PCI_ANY_ID, PCI_ANY_ID },	/* 3com */
+	{ 0x10b7, 0x0013, PCI_ANY_ID, PCI_ANY_ID },	/* 3com 3CRDAG675 */
 	{ 0x168c, 0x1014, PCI_ANY_ID, PCI_ANY_ID },	/* IBM minipci 5212 */
 	{ 0 }
 };
