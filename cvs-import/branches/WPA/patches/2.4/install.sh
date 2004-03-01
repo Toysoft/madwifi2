ab961441b5dc8b780f7781cb5000988ebccd8ba1
samleffler <samleffler@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2004-03-01 21:53:58 +0000

    Revise patch stuff to better support multiple OS versions.  Most of this is just moving files around in the repo (this should be done directly but because this is sourceforge we're forced to leave all sorts of turds around and lose our CVS history which thankfully is short).
    Submitted by:	Guy Erb

--- cvs-import/branches/WPA/patches/2.4/install.sh
+++ cvs-import/branches/WPA/patches/2.4/install.sh
@@ -8,6 +8,8 @@
 KERNEL_VERSION=`uname -r`
 KERNEL_PATH=${1:-/lib/modules/${KERNEL_VERSION}/build}
 
+DEPTH=../..
+
 MKDIR()
 {
 	DIR=$1
@@ -29,46 +31,50 @@ WIRELESS=${KERNEL_PATH}/drivers/net/wireless
 ATH=${WIRELESS}/ath
 MKDIR ${ATH}
 echo "Copy ath driver bits..."
-FILES=`ls ../ath/*.[ch] | sed '/mod.c/d'`
+FILES=`ls ${DEPTH}/ath/*.[ch] | sed '/mod.c/d'`
 INSTALL ${ATH} ${FILES}
-INSTALL ${ATH}/Makefile ../ath/Makefile.kernel
+INSTALL ${ATH}/Makefile ${DEPTH}/ath/Makefile.kernel-${KERNEL_VERSION}
 
-ATH_HAL=${WIRELESS}/ath_hal
+ATH_HAL=${WIRELESS}/_ath_hal
 MKDIR ${ATH_HAL}
 echo "Copy ath_hal bits..."
-INSTALL ${ATH_HAL}/Makefile ../ath_hal/Makefile.kernel
+INSTALL ${ATH_HAL}/Makefile ${DEPTH}/ath_hal/Makefile.kernel-${KERNEL_VERSION}
 
 MKDIR ${WIRELESS}/hal
+MKDIR ${WIRELESS}/hal/linux
 echo "Copy hal bits..."
-INSTALL ${WIRELESS} -r ../hal
+INSTALL ${WIRELESS}/hal ${DEPTH}/hal/*
+INSTALL ${WIRELESS}/hal/linux ${DEPTH}/hal/linux/*
 
-if [ -f Kconfig.wireless-${KERNEL_VERSION}.patch ]; then
-	PATCH ${WIRELESS}/Kconfig Kconfig.wireless-${KERNEL_VERSION}
+if [ -f Config.in.wireless-${KERNEL_VERSION}.patch ]; then
+	PATCH ${WIRELESS}/Config.in Config.in.wireless-${KERNEL_VERSION}
 else
 	echo "No patch file for your ${KERNEL_VERSION} kernel."
-	echo "Look at Kconfig.wireless to see how to change"
-	echo "${WIRELESS}/Kconfig to suit."
+	echo "Look at Config.in.wireless to see how to change"
+	echo "${WIRELESS}/Config.in to suit."
 fi
 PATCH ${WIRELESS}/Makefile Makefile.wireless
 
 NET=${KERNEL_PATH}/net
 NET80211=${NET}/net80211
+DOC=${KERNEL_PATH}/Documentation
 MKDIR ${NET80211}
 echo "Copy net80211 bits..."
-FILES=`ls ../net80211/*.[ch] | sed '/mod.c/d'`
+FILES=`ls ${DEPTH}/net80211/*.[ch] | sed '/mod.c/d'`
 INSTALL ${NET80211} ${FILES}
-INSTALL ${NET80211}/Makefile ../net80211/Makefile.kernel 
+INSTALL ${NET80211}/Makefile ${DEPTH}/net80211/Makefile.kernel-${KERNEL_VERSION}
 MKDIR ${NET80211}/compat
 echo "Setting up compatibility bits..."
-INSTALL ${NET80211}/compat ../include/compat.h
+INSTALL ${NET80211}/compat ${DEPTH}/include/compat.h
 MKDIR ${NET80211}/compat/sys
-INSTALL ${NET80211}/compat/sys ../include/sys/*.h
+INSTALL ${NET80211}/compat/sys ${DEPTH}/include/sys/*.h
 
-if [ -f Kconfig.net-${KERNEL_VERSION}.patch ]; then
-	PATCH ${NET}/Kconfig Kconfig.net-${KERNEL_VERSION}
+if [ -f Config.in.net-${KERNEL_VERSION}.patch ]; then
+	PATCH ${NET}/Config.in Config.in.net-${KERNEL_VERSION}
 else
 	echo "No patch file for your ${KERNEL_VERSION} kernel."
-	echo "Look at Kconfig.net to see how to change"
-	echo "${NET}/Kconfig to suit."
+	echo "Look at Config.in.net to see how to change"
+	echo "${NET}/Config.in to suit."
 fi
 PATCH ${NET}/Makefile Makefile.net
+PATCH ${DOC}/Configure.help Configure.help
