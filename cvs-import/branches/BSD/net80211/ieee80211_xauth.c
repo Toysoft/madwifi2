ad3dcb76cd23984b69620025df5b37b1fe2e36b7
togg <togg@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-02-19 20:27:53 +0000

    - update copyright notices

--- cvs-import/branches/BSD/net80211/ieee80211_xauth.c
+++ cvs-import/branches/BSD/net80211/ieee80211_xauth.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2004 Video54 Technologies, Inc.
+ * Copyright (c) 2004-2005 Video54 Technologies, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
