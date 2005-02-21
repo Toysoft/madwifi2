b8e6df9f15b125dd500e2109a97827d98c37b6d7
otaku <otaku@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-02-21 07:01:18 +0000

    Fixed initial commit, some files were left out. Thanks to Brian for the hint.

---
+++ cvs-import/branches/WDS/ath/radar.h
@@ -0,0 +1,14 @@
+#ifndef __RADAR_H__
+#define __RADAR_H__
+
+/*
+
+ 
+
+*/
+
+extern struct ieee80211_channel *radar_handle_interference(struct ieee80211com *ic);
+extern void radar_init(void);
+
+#endif
+
