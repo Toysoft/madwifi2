d5dd55aed0b203c2ef3cce47e87da64c70a854f1
br1 <br1@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-03-18 11:04:35 +0000

    fix compiler warning, sc_rxbufsize is a normal int

--- cvs-import/branches/BSD/ath/if_ath.c
+++ cvs-import/branches/BSD/ath/if_ath.c
@@ -2552,7 +2552,7 @@ ath_rxbuf_init(struct ath_softc *sc, struct ath_buf *bf)
 					    sc->sc_cachelsz - 1);
  			if (skb == NULL) {
  				DPRINTF(sc, ATH_DEBUG_ANY,
-					"%s: skbuff alloc of size %lu failed\n",
+					"%s: skbuff alloc of size %d failed\n",
 					__func__,
 					sc->sc_rxbufsize
 					+ sizeof(wlan_ng_prism2_header)
