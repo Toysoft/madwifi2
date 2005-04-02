4b3a5132039b7655404b8bec198668fb25073aef
togg <togg@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-04-02 19:11:14 +0000

    - nuke unintentional use of HAL_BOOL type   Obtained from: freebsd

--- cvs-import/branches/BSD/ath_rate/onoe/onoe.c
+++ cvs-import/branches/BSD/ath_rate/onoe/onoe.c
@@ -117,7 +117,7 @@ EXPORT_SYMBOL(ath_rate_node_cleanup);
 
 void
 ath_rate_findrate(struct ath_softc *sc, struct ath_node *an,
-	HAL_BOOL shortPreamble, size_t frameLen,
+	int shortPreamble, size_t frameLen,
 	u_int8_t *rix, int *try0, u_int8_t *txrate)
 {
 	struct onoe_node *on = ATH_NODE_ONOE(an);
@@ -133,7 +133,7 @@ EXPORT_SYMBOL(ath_rate_findrate);
 
 void
 ath_rate_setupxtxdesc(struct ath_softc *sc, struct ath_node *an,
-	struct ath_desc *ds, HAL_BOOL shortPreamble, u_int8_t rix)
+	struct ath_desc *ds, int shortPreamble, u_int8_t rix)
 {
 	struct onoe_node *on = ATH_NODE_ONOE(an);
 
