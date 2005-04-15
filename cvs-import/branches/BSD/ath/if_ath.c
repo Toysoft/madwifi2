97a074fe5ad8350e84d052ff7f2b518faceb3c83
jbicket <jbicket@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-04-15 04:39:50 +0000

    Add raw 802.11 packet injection to athXraw device using ath_start_raw.

--- cvs-import/branches/BSD/ath/if_ath.c
+++ cvs-import/branches/BSD/ath/if_ath.c
@@ -270,6 +270,56 @@ MODULE_PARM_DESC(countrycode, "Override default country code");
 MODULE_PARM_DESC(outdoor, "Enable/disable outdoor use");
 MODULE_PARM_DESC(xchanmode, "Enable/disable extended channel mode");
 
+
+/*
+ * For packet capture, define the same physical layer packet header 
+ * structure as used in the wlan-ng driver 
+ */
+enum {
+	DIDmsg_lnxind_wlansniffrm		= 0x00000044,
+	DIDmsg_lnxind_wlansniffrm_hosttime	= 0x00010044,
+	DIDmsg_lnxind_wlansniffrm_mactime	= 0x00020044,
+	DIDmsg_lnxind_wlansniffrm_channel	= 0x00030044,
+	DIDmsg_lnxind_wlansniffrm_rssi		= 0x00040044,
+	DIDmsg_lnxind_wlansniffrm_sq		= 0x00050044,
+	DIDmsg_lnxind_wlansniffrm_signal	= 0x00060044,
+	DIDmsg_lnxind_wlansniffrm_noise		= 0x00070044,
+	DIDmsg_lnxind_wlansniffrm_rate		= 0x00080044,
+	DIDmsg_lnxind_wlansniffrm_istx		= 0x00090044,
+	DIDmsg_lnxind_wlansniffrm_frmlen	= 0x000A0044
+};
+enum {
+	P80211ENUM_msgitem_status_no_value	= 0x00
+};
+enum {
+	P80211ENUM_truth_false			= 0x00,
+	P80211ENUM_truth_true			= 0x01
+};
+
+typedef struct {
+	u_int32_t did;
+	u_int16_t status;
+	u_int16_t len;
+	u_int32_t data;
+} p80211item_uint32_t;
+
+typedef struct {
+	u_int32_t msgcode;
+	u_int32_t msglen;
+#define WLAN_DEVNAMELEN_MAX 16
+	u_int8_t devname[WLAN_DEVNAMELEN_MAX];
+	p80211item_uint32_t hosttime;
+	p80211item_uint32_t mactime;
+	p80211item_uint32_t channel;
+	p80211item_uint32_t rssi;
+	p80211item_uint32_t sq;
+	p80211item_uint32_t signal;
+	p80211item_uint32_t noise;
+	p80211item_uint32_t rate;
+	p80211item_uint32_t istx;
+	p80211item_uint32_t frmlen;
+} wlan_ng_prism2_header;
+
 int
 ath_attach(u_int16_t devid, struct net_device *dev)
 {
@@ -1053,6 +1103,9 @@ ath_stop_locked(struct net_device *dev)
 		 */
 		ieee80211_new_state(ic, IEEE80211_S_INIT, -1);
 		netif_stop_queue(dev);
+		if (sc->sc_rawdev_enabled) 
+			netif_stop_queue(&sc->sc_rawdev);
+
 		dev->flags &= ~IFF_RUNNING;
 		if (!sc->sc_invalid) {
 			if (sc->sc_softled) {
@@ -1150,9 +1203,262 @@ ath_reset(struct net_device *dev)
 
 	if (ic->ic_state == IEEE80211_S_RUN)
 		netif_wake_queue(dev);	/* restart xmit */
+
+	if (sc->sc_rawdev_enabled)
+		netif_wake_queue(&sc->sc_rawdev);
 	return 0;
 }
 
+/*
+ * ath_start for raw 802.11 packets.
+ */
+static int
+ath_start_raw(struct sk_buff *skb, struct net_device *dev)
+{
+#define	CTS_DURATION \
+	ath_hal_computetxtime(ah, rt, IEEE80211_ACK_LEN, cix, AH_TRUE)
+#define	updateCTSForBursting(_ah, _ds, _txq) \
+	ath_hal_updateCTSForBursting(_ah, _ds, \
+	    _txq->axq_linkbuf != NULL ? _txq->axq_linkbuf->bf_desc : NULL, \
+	    _txq->axq_lastdsWithCTS, _txq->axq_gatingds, \
+	    txopLimit, CTS_DURATION)
+	struct ath_softc *sc = dev->priv;
+	struct ieee80211com *ic = &sc->sc_ic;
+	struct ath_hal *ah = sc->sc_ah;
+	const struct chanAccParams *cap = &ic->ic_wme.wme_chanParams;
+	struct ath_txq *txq;
+	struct ath_buf *bf;
+	HAL_PKT_TYPE atype;
+	int pktlen, hdrlen, try0, pri, dot11Rate; 
+	u_int8_t ctsrate, ctsduration, txrate;
+	u_int8_t cix = 0xff;         /* NB: silence compiler */
+	u_int flags = 0;
+	struct ieee80211_frame *wh; 
+	struct ath_desc *ds;
+	const HAL_RATE_TABLE *rt;
+
+	if ((sc->sc_dev.flags & IFF_RUNNING) == 0 || sc->sc_invalid) {
+		/* device is not up... silently discard packet */
+		dev_kfree_skb(skb);
+		return 0;
+	}
+
+	/*
+	 * Grab a TX buffer and associated resources.
+	 */
+	ATH_TXBUF_LOCK_BH(sc);
+	bf = STAILQ_FIRST(&sc->sc_txbuf);
+	if (bf != NULL)
+		STAILQ_REMOVE_HEAD(&sc->sc_txbuf, bf_list);
+	/* XXX use a counter and leave at least one for mgmt frames */
+	if (STAILQ_EMPTY(&sc->sc_txbuf)) {
+		DPRINTF(sc, ATH_DEBUG_XMIT, "%s: stop queue\n", __func__);
+		sc->sc_stats.ast_tx_qstop++;
+		netif_stop_queue(&sc->sc_dev);
+		netif_stop_queue(&sc->sc_rawdev);
+	}
+	ATH_TXBUF_UNLOCK_BH(sc);
+	
+	if (bf == NULL) {		/* NB: should not happen */
+		DPRINTF(sc, ATH_DEBUG_ANY, "%s: out of xmit buffers\n",
+			__func__);
+		sc->sc_stats.ast_tx_nobuf++;
+		dev_kfree_skb(skb);
+		return 0;
+	}
+
+	rt = sc->sc_currates;
+	KASSERT(rt != NULL, ("no rate table, mode %u", sc->sc_curmode));
+	flags = HAL_TXDESC_INTREQ | HAL_TXDESC_CLRDMASK;
+	try0 = ATH_TXMAXTRY;
+	dot11Rate = 0;
+	ctsrate = 0;
+	ctsduration = 0;
+	pri = 0;
+	txq = sc->sc_ac2q[pri];
+	txrate = rt->info[0].rateCode;
+	atype =  HAL_PKT_TYPE_NORMAL;
+
+	/*
+	 * strip any physical layer header off the skb, if it is
+	 * present, and read out any settings we can, like what txrate
+	 * to send this packet at.
+	 */
+	switch(dev->type) {
+	case ARPHRD_IEEE80211:
+		break;
+	case ARPHRD_IEEE80211_PRISM: {
+		wlan_ng_prism2_header *ph = NULL;
+		ph = (wlan_ng_prism2_header *) skb->data;
+		/* does it look like there is a prism header here? */
+		if (skb->len > sizeof (wlan_ng_prism2_header) &&
+		    ph->msgcode == DIDmsg_lnxind_wlansniffrm &&
+		    ph->rate.did == DIDmsg_lnxind_wlansniffrm_rate) {
+			dot11Rate = ph->rate.data;
+			skb_pull(skb, sizeof(wlan_ng_prism2_header));
+		} 
+		break;
+	}
+	case ARPHRD_IEEE80211_RADIOTAP:
+		break;
+	default:
+		/* nothing */
+		break;
+	}
+	
+	if (dot11Rate != 0) {
+		int index = sc->sc_rixmap[dot11Rate & IEEE80211_RATE_VAL];
+		if (index >= 0 && index < rt->rateCount) {
+			txrate = rt->info[index].rateCode;
+		}
+	}
+
+	wh = (struct ieee80211_frame *) skb->data;
+	pktlen = skb->len + IEEE80211_CRC_LEN;
+	hdrlen = sizeof(struct ieee80211_frame);
+
+	if (hdrlen < pktlen) 
+		hdrlen = pktlen;
+
+	if (IEEE80211_IS_MULTICAST(wh->i_addr1)) {
+		flags |= HAL_TXDESC_NOACK;	/* no ack on broad/multicast */
+		sc->sc_stats.ast_tx_noack++;
+	}
+	
+	if (IFF_DUMPPKTS(sc, ATH_DEBUG_XMIT))
+		ieee80211_dump_pkt(skb->data, skb->len,
+				   sc->sc_hwmap[txrate].ieeerate, -1);
+
+	/*
+	 * Load the DMA map so any coalescing is done.  This
+	 * also calculates the number of descriptors we need.
+	 */
+	bf->bf_skbaddr = bus_map_single(sc->sc_bdev,
+		skb->data, pktlen, BUS_DMA_TODEVICE);
+	DPRINTF(sc, ATH_DEBUG_XMIT, "%s: skb %p [data %p len %u] skbaddr %lx\n",
+		__func__, skb, skb->data, skb->len, (long unsigned int) bf->bf_skbaddr);
+	if (BUS_DMA_MAP_ERROR(bf->bf_skbaddr)) {
+		if_printf(dev, "%s: DMA mapping failed\n", __func__);
+		dev_kfree_skb(skb);
+		bf->bf_skb = NULL;
+		sc->sc_stats.ast_tx_busdma++;
+
+		ATH_TXBUF_LOCK_BH(sc);
+		STAILQ_INSERT_TAIL(&sc->sc_txbuf, bf, bf_list);
+		ATH_TXBUF_UNLOCK_BH(sc);
+		return -EIO;
+	}
+	bf->bf_skb = skb;
+	bf->bf_node = NULL;
+
+	/* setup descriptors */
+	ds = bf->bf_desc;
+	rt = sc->sc_currates;
+	KASSERT(rt != NULL, ("no rate table, mode %u", sc->sc_curmode));
+
+	/*
+	 * Formulate first tx descriptor with tx controls.
+	 */
+	/* XXX check return value? */
+	ath_hal_setuptxdesc(ah, ds
+		, pktlen		/* packet length */
+		, hdrlen		/* header length */
+		, atype			/* Atheros packet type */
+		, 60                 	/* txpower */
+		, txrate, try0		/* series 0 rate/tries */
+		, HAL_TXKEYIX_INVALID	/* key cache index */
+		, sc->sc_txantenna	/* antenna mode */
+		, flags			/* flags */
+		, ctsrate		/* rts/cts rate */
+		, ctsduration		/* rts/cts duration */
+	);
+
+	/*
+	 * Fillin the remainder of the descriptor info.
+	 */
+	ds->ds_link = 0;
+	ds->ds_data = bf->bf_skbaddr;
+	ath_hal_filltxdesc(ah, ds
+		, skb->len		/* segment length */
+		, AH_TRUE		/* first segment */
+		, AH_TRUE		/* last segment */
+		, ds			/* first descriptor */
+	);
+
+	DPRINTF(sc, ATH_DEBUG_XMIT, "%s: Q%d: %08x %08x %08x %08x %08x %08x\n",
+	    __func__, txq->axq_qnum, ds->ds_link, ds->ds_data,
+	    ds->ds_ctl0, ds->ds_ctl1, ds->ds_hw[0], ds->ds_hw[1]);
+	/*
+	 * Insert the frame on the outbound list and
+	 * pass it on to the hardware.
+	 */
+	ATH_TXQ_LOCK_BH(txq);
+	if (flags & (HAL_TXDESC_RTSENA | HAL_TXDESC_CTSENA)) {
+		u_int32_t txopLimit = IEEE80211_TXOP_TO_US(
+			cap->cap_wmeParams[pri].wmep_txopLimit);
+		/*
+		 * When bursting, potentially extend the CTS duration
+		 * of a previously queued frame to cover this frame
+		 * and not exceed the txopLimit.  If that can be done
+		 * then disable RTS/CTS on this frame since it's now
+		 * covered (burst extension).  Otherwise we must terminate
+		 * the burst before this frame goes out so as not to
+		 * violate the WME parameters.  All this is complicated
+		 * as we need to update the state of packets on the
+		 * (live) hardware queue.  The logic is buried in the hal
+		 * because it's highly chip-specific.
+		 */
+		if (txopLimit != 0) {
+			sc->sc_stats.ast_tx_ctsburst++;
+			if (updateCTSForBursting(ah, ds, txq) == 0) {
+				/*
+				 * This frame was not covered by RTS/CTS from
+				 * the previous frame in the burst; update the
+				 * descriptor pointers so this frame is now
+				 * treated as the last frame for extending a
+				 * burst.
+				 */
+				txq->axq_lastdsWithCTS = ds;
+				/* set gating Desc to final desc */
+				txq->axq_gatingds =
+					(struct ath_desc *)txq->axq_link;
+			} else
+				sc->sc_stats.ast_tx_ctsext++;
+		}
+	}
+	ATH_TXQ_INSERT_TAIL(txq, bf, bf_list);
+	DPRINTF(sc, ATH_DEBUG_TX_PROC, "%s: txq depth = %d\n",
+		__func__, txq->axq_depth);
+	if (txq->axq_link == NULL) {
+		ath_hal_puttxbuf(ah, txq->axq_qnum, bf->bf_daddr);
+		DPRINTF(sc, ATH_DEBUG_XMIT,
+			"%s: TXDP[%u] = %p (%p) depth %d\n", __func__,
+			txq->axq_qnum, (caddr_t)bf->bf_daddr, bf->bf_desc,
+			txq->axq_depth);
+	} else {
+		*txq->axq_link = bf->bf_daddr;
+		DPRINTF(sc, ATH_DEBUG_XMIT,
+			"%s: link[%u](%p)=%p (%p) depth %d\n", __func__,
+			txq->axq_qnum, txq->axq_link,
+			(caddr_t)bf->bf_daddr, bf->bf_desc, txq->axq_depth);
+	}
+	txq->axq_link = &bf->bf_desc->ds_link;
+	/*
+	 * The CAB queue is started from the SWBA handler since
+	 * frames only go out on DTIM and to avoid possible races.
+	 */
+	if (txq != sc->sc_cabq)
+		ath_hal_txstart(ah, txq->axq_qnum);
+	ATH_TXQ_UNLOCK_BH(txq);
+
+	sc->sc_devstats.tx_packets++;
+	sc->sc_devstats.tx_bytes += skb->len;
+	dev->trans_start = jiffies;
+
+	return 0;
+#undef updateCTSForBursting
+#undef CTS_DURATION
+}
 
 static int
 ath_start(struct sk_buff *skb, struct net_device *dev)
@@ -1200,6 +1506,8 @@ ath_start(struct sk_buff *skb, struct net_device *dev)
 			DPRINTF(sc, ATH_DEBUG_XMIT, "%s: stop queue\n", __func__);
 			sc->sc_stats.ast_tx_qstop++;
 			netif_stop_queue(dev);
+			if (sc->sc_rawdev_enabled)
+				netif_stop_queue(&sc->sc_rawdev);
 		}
 		ATH_TXBUF_UNLOCK_BH(sc);
 
@@ -1750,6 +2058,8 @@ ath_key_update_begin(struct ieee80211com *ic)
 		tasklet_disable(&sc->sc_rxtq);
 #endif
 	netif_stop_queue(dev);	// TODO: find a way to not block mgmt frames
+	if (sc->sc_rawdev_enabled) 
+		netif_stop_queue(&sc->sc_rawdev);
 }
 
 static void
@@ -1761,6 +2071,8 @@ ath_key_update_end(struct ieee80211com *ic)
 	DPRINTF(sc, ATH_DEBUG_FATAL, "%lu %s (%s)\n", jiffies, __func__, dev->name);
 	DPRINTF(sc, ATH_DEBUG_KEYCACHE, "%s:\n", __func__);
 	netif_start_queue(dev);
+	if (sc->sc_rawdev_enabled) 
+		netif_start_queue(&sc->sc_rawdev);
 #if 1
 	if (!in_softirq())		/* NB: see above */
 		tasklet_enable(&sc->sc_rxtq);
@@ -2501,55 +2813,6 @@ ath_node_getrssi(const struct ieee80211_node *ni)
 #undef HAL_EP_RND
 }
 
-/*
- * For packet capture, define the same physical layer packet header 
- * structure as used in the wlan-ng driver 
- */
-enum {
-	DIDmsg_lnxind_wlansniffrm		= 0x00000044,
-	DIDmsg_lnxind_wlansniffrm_hosttime	= 0x00010044,
-	DIDmsg_lnxind_wlansniffrm_mactime	= 0x00020044,
-	DIDmsg_lnxind_wlansniffrm_channel	= 0x00030044,
-	DIDmsg_lnxind_wlansniffrm_rssi		= 0x00040044,
-	DIDmsg_lnxind_wlansniffrm_sq		= 0x00050044,
-	DIDmsg_lnxind_wlansniffrm_signal	= 0x00060044,
-	DIDmsg_lnxind_wlansniffrm_noise		= 0x00070044,
-	DIDmsg_lnxind_wlansniffrm_rate		= 0x00080044,
-	DIDmsg_lnxind_wlansniffrm_istx		= 0x00090044,
-	DIDmsg_lnxind_wlansniffrm_frmlen	= 0x000A0044
-};
-enum {
-	P80211ENUM_msgitem_status_no_value	= 0x00
-};
-enum {
-	P80211ENUM_truth_false			= 0x00,
-	P80211ENUM_truth_true			= 0x01
-};
-
-typedef struct {
-	u_int32_t did;
-	u_int16_t status;
-	u_int16_t len;
-	u_int32_t data;
-} p80211item_uint32_t;
-
-typedef struct {
-	u_int32_t msgcode;
-	u_int32_t msglen;
-#define WLAN_DEVNAMELEN_MAX 16
-	u_int8_t devname[WLAN_DEVNAMELEN_MAX];
-	p80211item_uint32_t hosttime;
-	p80211item_uint32_t mactime;
-	p80211item_uint32_t channel;
-	p80211item_uint32_t rssi;
-	p80211item_uint32_t sq;
-	p80211item_uint32_t signal;
-	p80211item_uint32_t noise;
-	p80211item_uint32_t rate;
-	p80211item_uint32_t istx;
-	p80211item_uint32_t frmlen;
-} wlan_ng_prism2_header;
-
 static int
 ath_rxbuf_init(struct ath_softc *sc, struct ath_buf *bf)
 {
@@ -2756,7 +3019,7 @@ ath_tx_capture(struct net_device *dev, struct ath_desc *ds, struct sk_buff *skb)
 		ph->rate.did = DIDmsg_lnxind_wlansniffrm_rate;
 		ph->rate.status = 0;
 		ph->rate.len = 4;
-		ph->rate.data = sc->sc_hwmap[ds->ds_rxstat.rs_rate].ieeerate;
+		ph->rate.data = sc->sc_hwmap[ds->ds_txstat.ts_rate &~ HAL_TXSTAT_ALTRATE].ieeerate;
 		break;
 	}
 	case ARPHRD_IEEE80211_RADIOTAP: {
@@ -3211,7 +3474,8 @@ rx_accept:
 			goto rx_next;
 		}
 
-		if (sc->sc_rawdev_enabled && (sc->sc_rawdev.flags & IFF_UP)) {
+		if (sc->sc_rawdev_enabled && 
+		    ((sc->sc_rawdev.flags & (IFF_RUNNING|IFF_UP)) == (IFF_RUNNING|IFF_UP))) {
 			struct sk_buff *skb2;
 			skb2 = skb_clone(skb, GFP_ATOMIC);
 			if (skb2) {
@@ -4059,6 +4323,9 @@ ath_tx_tasklet_q0(TQUEUE_ARG data)
 	/* XXX use a low watermark to reduce wakeups */
 	if (ic->ic_state == IEEE80211_S_RUN)
 		netif_wake_queue(dev);
+
+	if (sc->sc_rawdev_enabled)
+		netif_wake_queue(&sc->sc_rawdev);
 }
 
 /*
@@ -4094,6 +4361,9 @@ ath_tx_tasklet_q0123(TQUEUE_ARG data)
 	/* XXX use a low watermark to reduce wakeups */
 	if (ic->ic_state == IEEE80211_S_RUN)
 		netif_wake_queue(dev);
+
+	if (sc->sc_rawdev_enabled)
+		netif_wake_queue(&sc->sc_rawdev);
 }
 
 /*
@@ -4129,6 +4399,10 @@ ath_tx_tasklet(TQUEUE_ARG data)
 	/* XXX use a low watermark to reduce wakeups */
 	if (ic->ic_state == IEEE80211_S_RUN)
 		netif_wake_queue(dev);
+
+	if (sc->sc_rawdev_enabled)
+		netif_wake_queue(&sc->sc_rawdev);
+
 }
 
 static void
@@ -4218,6 +4492,9 @@ ath_draintxq(struct ath_softc *sc)
 	}
 	sc->sc_dev.trans_start = jiffies;
 	netif_start_queue(&sc->sc_dev);		// TODO: needed here?
+	if (sc->sc_rawdev_enabled)
+		netif_start_queue(&sc->sc_rawdev);
+	
 	for (i = 0; i < HAL_NUM_TX_QUEUES; i++) {
 		if (ATH_TXQ_SETUP(sc, i))
 			ath_tx_draintxq(sc, &sc->sc_txq[i]);
@@ -4492,6 +4769,8 @@ ath_newstate(struct ieee80211com *ic, enum ieee80211_state nstate, int arg)
 	del_timer(&sc->sc_cal_ch);		/* periodic calibration timer */
 	ath_hal_setledstate(ah, leds[nstate]);	/* set LED */
 	netif_stop_queue(dev);			/* before we do anything else */
+	if (sc->sc_rawdev_enabled)
+		netif_stop_queue(&sc->sc_rawdev);
 
 	if (nstate == IEEE80211_S_INIT) {
 		sc->sc_imask &= ~(HAL_INT_SWBA | HAL_INT_BMISS);
@@ -4593,6 +4872,9 @@ done:
 	}
 bad:
 	netif_start_queue(dev);
+	if (sc->sc_rawdev_enabled) 
+		netif_start_queue(&sc->sc_rawdev);
+
 	return error;
 }
 
@@ -4872,7 +5154,7 @@ ath_rawdev_attach(struct ath_softc *sc)
 	rawdev->type = t;
 
 	rawdev->stop = NULL;
-	rawdev->hard_start_xmit = NULL;
+	rawdev->hard_start_xmit = ath_start_raw;
 	rawdev->set_multicast_list = NULL;
 	rawdev->get_stats = ath_getstats;
 	rawdev->tx_queue_len = 0;
@@ -4882,7 +5164,12 @@ ath_rawdev_attach(struct ath_softc *sc)
 	if (register_netdev(rawdev)) {
 		goto bad;
 	}
+
+	if ((sc->sc_dev.flags & (IFF_RUNNING|IFF_UP)) == (IFF_RUNNING|IFF_UP))
+		netif_start_queue(&sc->sc_rawdev);
+
 	sc->sc_rawdev_enabled = 1;
+
 	return 0;
  bad:
 	return -1;
@@ -4893,6 +5180,7 @@ ath_rawdev_detach(struct ath_softc *sc)
 {
 	if (sc->sc_rawdev_enabled) {
 		sc->sc_rawdev_enabled = 0;
+		netif_stop_queue(&sc->sc_rawdev);
 		unregister_netdev(&sc->sc_rawdev);
 	}
 }
