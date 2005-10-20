6391477b9557751e90f08ec74e048c1b421d9a0f
br1 <br1@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-10-20 10:58:24 +0000

    try to define pm_message_t and msecs_to_jiffies for the right kernel versions

--- trunk/ath/if_ath_pci.h
+++ trunk/ath/if_ath_pci.h
@@ -43,6 +43,7 @@
 #include <linux/pci.h>
 #define bus_map_single		pci_map_single
 #define bus_unmap_single	pci_unmap_single
+
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,9)
 #define bus_dma_sync_single	pci_dma_sync_single_for_cpu
 #define	PCI_SAVE_STATE(a,b)	pci_save_state(a)
@@ -52,9 +53,14 @@
 #define	PCI_SAVE_STATE(a,b)	pci_save_state(a,b)
 #define	PCI_RESTORE_STATE(a,b)	pci_restore_state(a,b)
 #endif
+
 #define bus_alloc_consistent	pci_alloc_consistent
 #define bus_free_consistent	pci_free_consistent
 #define BUS_DMA_FROMDEVICE	PCI_DMA_FROMDEVICE
 #define BUS_DMA_TODEVICE	PCI_DMA_TODEVICE
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,11)
+#define pm_message_t u32
+#endif
 
 #endif   /* _DEV_ATH_PCI_H_ */
