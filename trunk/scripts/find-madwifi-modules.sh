eeb29be5fc8f62975b30078bc6b61a00e4b48eba
mrenzmann <mrenzmann@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-11-01 10:28:03 +0000

    Changes related to ticket #16:
     * introduced script find-madwifi-modules.sh which searches in a
       given directory for madwifi-related modules; this is used to
       warn the user about possible "mixed installations" (where old
       and new modules are mixed up and cause strange problems)
     * introduced new target 'sanitycheck'; currently checks for
       uudecode (only if UUDECODE is not manually specified) for
       old madwifi-related modules in the target directory (see
       above) 
     * target 'dotconfig' assimilated by target 'sanitycheck'

---
+++ trunk/scripts/find-madwifi-modules.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+function usage {
+    echo "Purpose:"
+    echo "Locate all madwifi-related kernel modules in a given directory"
+    echo "(including all its subdirectories)."
+    echo
+    echo "Usage:"
+    echo "$0 <dir>"
+    echo
+    echo "<dir>: name of the directory used for the search"
+    echo
+}
+
+if [ "$#" -ne 1 ]; then
+    usage >&2
+    exit 1
+fi
+
+SEARCH=$1
+
+if [ ! -d "$SEARCH" ]; then
+    echo "ERROR: $1 is no valid directory" >&2
+    exit 1
+fi
+
+PATTERN="^.*\/(ath_(hal|pci|rate_(onoe|amrr|sample))\.k?o)|(wlan(_(acl|ccmp|scan_(ap|sta)|tkip|wep|xauth))?\.k?o)$"
+
+find $SEARCH -type f -regex '.*\.k?o' | \
+grep -w -E "$PATTERN" > /dev/null 2>&1 && {
+    echo
+    echo "WARNING:"
+    echo "It seems that there are modules left from previous MadWifi installations."
+    echo "You should consider to remove them before you continue, else you might"
+    echo "experience problems during operation."
+    echo
+    
+    read -p "Continue anyway? [y,N] "
+    case $REPLY in
+        y|Y)
+		exit 0
+		;;
+	*)
+		exit 1
+		;;
+    esac
+}
+
