242026750ca6bc1e170b4d92876682ef5dba4e97
br1 <br1@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-11-01 19:13:00 +0000

    fix 80211debug - use right interface name and proc file

--- trunk/tools/80211debug.c
+++ trunk/tools/80211debug.c
@@ -189,7 +189,7 @@ sysctlbyname(const char *oid0, void *oldp, size_t *oldlenp, void *newp, size_t n
 int
 main(int argc, char *argv[])
 {
-	const char *ifname = "wlan0";
+	const char *ifname = "wifi0";
 	const char *cp, *tp;
 	const char *sep;
 	int c, op, i;
@@ -212,9 +212,9 @@ main(int argc, char *argv[])
 	}
 
 #ifdef __linux__
-	snprintf(oid, sizeof(oid), "net.%s.debug", ifname);
+	snprintf(oid, sizeof(oid), "dev.%s.debug", ifname);
 #else
-	snprintf(oid, sizeof(oid), "net.wlan.%s.debug", ifname+4);
+	snprintf(oid, sizeof(oid), "dev.wlan.%s.debug", ifname+4);
 #endif
 	debuglen = sizeof(debug);
 	if (sysctlbyname(oid, &debug, &debuglen, NULL, 0) < 0)
