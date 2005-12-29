73cc4d1d56db4e5745f09098e3720cd7580fab5a
mrenzmann <mrenzmann@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2005-12-29 15:43:36 +0000

    KERNELPATH determination is changed to avoid the use of readlink (which probably isn't available on all systems).
    KERNELRELEASE determination now adapts a trick that is used by
    the lm_sensors project. A preprocessed version of kernelversion.c
    is parsed to get the proper value on systems that have multiple
    UTS_RELEASE definitions in <linux/version.h> (Mandrake, for
    example).
    
    This commit fixes ticket #101.
    
    Signed-off-by: Michael Renzmann <mrenzmann@otaku42.de>

---
+++ trunk/kernelversion.c
@@ -0,0 +1,8 @@
+/* This file is used for a trick to determine the version of the kernel
+ * build tree. Simply grepping <linux/version.h> doesn't work, since
+ * some distributions have multiple UTS_RELEASE definitions in that
+ * file.
+ * Taken from the lm_sensors project.
+ */
+#include <linux/version.h>
+char *uts_release=UTS_RELEASE;
