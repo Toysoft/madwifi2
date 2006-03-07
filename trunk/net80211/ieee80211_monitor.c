6ca8cf1a7517917f73044180c4569b2783b7a70c
jbicket <jbicket@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2006-03-07 00:08:50 +0000

    add signal field in prism headers.

--- trunk/net80211/ieee80211_monitor.c
+++ trunk/net80211/ieee80211_monitor.c
@@ -264,7 +264,7 @@ ieee80211_input_monitor(struct ieee80211com *ic, struct sk_buff *skb,
 			ph->rssi.did = DIDmsg_lnxind_wlansniffrm_rssi;
 			ph->rssi.status = 0;
 			ph->rssi.len = 4;
-			ph->rssi.data = 0;
+			ph->rssi.data = signal;
 			
 			ph->noise.did = DIDmsg_lnxind_wlansniffrm_noise;
 			ph->noise.status = 0;
