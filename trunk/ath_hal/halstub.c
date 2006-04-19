ed1ed34c6c0ba20fb935f16d7162cb3feb62dcc9
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2006-04-19 07:26:14 +0000

    Fix warning about unresolved symbols in ath_hal module.
    The kernel build system reports unresolved symbols when ath_hal is built
    with fake hal.c (i.e. in the fisrt pass).  This patch puts missing
    symbols to hal.c.  The template for the fake hal.c is called halstub.c.
    
    This closes ticket #542

---
+++ trunk/ath_hal/halstub.c
@@ -0,0 +1,14 @@
+/*
+ * This file is here only to fool the build system into thinking that
+ * there is a source for hal.o
+ */
+
+void ath_hal_getwirelessmodes(void) {}
+void ath_hal_init_channels(void) {}
+char **ath_hal_buildopts;
+void ath_hal_probe(void) {}
+char *ath_hal_version;
+void ath_hal_mhz2ieee(void) {}
+void ath_hal_computetxtime(void) {}
+void ath_hal_attach(void) {}
+void ath_hal_process_noisefloor(void) {}
