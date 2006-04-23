cb4ace81cfdc0d064c95fd684979f1d796c161de
kelmo <kelmo@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2006-04-23 08:14:44 +0000

    Export module version information.
    Signed-off-by: Kel Modderman <kelrin@tpg.com.au>

--- trunk/ath/if_ath_ahb.c
+++ trunk/ath/if_ath_ahb.c
@@ -422,6 +422,7 @@ ath_ioctl_ethtool(struct ath_softc *sc, int cmd, void __user *addr)
 
 MODULE_AUTHOR("Atheros Communications, Inc.");
 MODULE_DESCRIPTION("Support for Atheros 802.11 wireless LAN cards.");
+MODULE_VERSION("svn "SVNVERSION);
 MODULE_SUPPORTED_DEVICE("Atheros WLAN cards");
 #ifdef MODULE_LICENSE
 MODULE_LICENSE("Dual BSD/GPL");
