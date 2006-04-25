2afca46b1a18abcd5f69b507cc54739b4bd72d17
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2006-04-25 19:40:32 +0000

    Don't use Linux includes just to have a definition for NULL
    Using any external includes can cause hal.c to be recompiled at the
    second stage, replacing the correct hal.o.
    
    This should close ticket #557.

--- trunk/ath_hal/hal.c
+++ trunk/ath_hal/hal.c
@@ -3,20 +3,16 @@
  * there is a source for hal.o
  */
 
-#include <linux/config.h>
-#include <linux/version.h>
-#include <linux/module.h>
-
 void ath_hal_getwirelessmodes(void) {}
 void ath_hal_init_channels(void) {}
-char *ath_hal_buildopts[] = { "dummy", NULL };
-char *ath_hal_probe(void) { return NULL; }
+char *ath_hal_buildopts[] = { "dummy", (char *) 0 };
+char *ath_hal_probe(void) { return (char *) 0; }
 char ath_hal_version[] = "dummy";
 void ath_hal_mhz2ieee(void) {}
 void ath_hal_computetxtime(void) {}
 void *ath_hal_attach(int a, void *b, int c, void *d, int *status)
 {
 	*status = 1;
-	return NULL;
+	return (void *) 0;
 }
 void ath_hal_process_noisefloor(void) {}
