e357bff71d14ca9c023054fd2596379fbe439ea9
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-01-03 07:26:55 +0000

    Annotate i_seq and ni_pschangeseq for endianess
    i_seq is part of the 802.11 frame and should not be cast to types with
    CPU-dependent endianess.  It's always treated as little-endian if cast
    to 16-bit types, so cast it to __le16.
    
    ni_pschangeseq always stores little-endian data, so make it __le16.

--- trunk/net80211/ieee80211_node.h
+++ trunk/net80211/ieee80211_node.h
@@ -169,7 +169,7 @@ struct ieee80211_node {
 	u_int8_t ni_uapsd;			/* U-APSD per-node flags matching WMM STA Qos Info field */
 	u_int8_t ni_uapsd_maxsp; 		/* maxsp from flags above */
 	u_int16_t ni_uapsd_trigseq[WME_NUM_AC]; 	/* trigger suppression on retry */
-	u_int16_t ni_pschangeseq;
+	__le16 ni_pschangeseq;
 };
 MALLOC_DECLARE(M_80211_NODE);
 
