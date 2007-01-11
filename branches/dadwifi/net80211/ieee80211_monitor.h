7662b4e960d22a918f339d3543f5de97c51c3270
dwhedon <dwhedon@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-01-11 17:38:21 +0000

    Merge revision 1886 from trunk.

--- branches/dadwifi/net80211/ieee80211_monitor.h
+++ branches/dadwifi/net80211/ieee80211_monitor.h
@@ -131,7 +131,7 @@ struct ath_rx_radiotap_header {
         u_int8_t wr_antenna;
         u_int8_t wr_antsignal;
 	u_int8_t wr_pad[2]; /* Ensure fcs is on 32 bit boundary */
-	__le16 wr_fcs;
+	__le32 wr_fcs;
 }__attribute__((__packed__));
 
 #define ATH_TX_RADIOTAP_PRESENT (               \
