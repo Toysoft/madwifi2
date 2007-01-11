b946e947135e881d84da61437d35d582625b8ccf
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-01-11 04:51:52 +0000

    Fix compilation on 2.6.20 - adapt to new workqueue API
    INIT_WORK doesn't take the third argument (data) in Linux 2.6.20.  The
    handler gets work_struct instead and obtains the rest of the data using
    structure offsets.
    
    Use this approach for radar detection.  Provide compatibility definition
    of container_of for Linux 2.4.
    
    Simplify the existing ATH_INIT_SCHED_TASK implementations to only take
    two arguments and to pass work_struct/tq_struct as the argument for
    consistency with Linux 2.6.20.

--- trunk/include/compat.h
+++ trunk/include/compat.h
@@ -49,6 +49,12 @@
 #endif
 #endif
 
+#ifndef container_of
+#define container_of(ptr, type, member) ({			\
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+#endif
+
 /*
  * BSD/Linux compatibility shims.  These are used mainly to
  * minimize differences when importing necesary BSD code.
