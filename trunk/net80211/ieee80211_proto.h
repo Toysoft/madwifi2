15766205d78f210dd98b3b32541c0d6a599dc6de
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-01-30 02:33:52 +0000

    Fix signedness of bo->bo_caps, found by sparse

--- trunk/net80211/ieee80211_proto.h
+++ trunk/net80211/ieee80211_proto.h
@@ -255,7 +255,7 @@ struct sk_buff *ieee80211_getcfframe(struct ieee80211vap *, int);
  * can update the frame later w/ minimal overhead.
  */
 struct ieee80211_beacon_offsets {
-	u_int16_t *bo_caps;		/* capabilities */
+	__le16 *bo_caps;		/* capabilities */
 	u_int8_t *bo_tim;		/* start of atim/dtim */
 	u_int8_t *bo_wme;		/* start of WME parameters */
 	u_int8_t *bo_tim_trailer;	/* start of fixed-size tim trailer */
