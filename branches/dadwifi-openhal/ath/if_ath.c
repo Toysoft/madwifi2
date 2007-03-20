4451bcb6235e037e8b11daeec57b9d2a17ec0a7c
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-03-20 06:04:40 +0000

    Remove useless checks for "val < 0", it's unsigned

--- branches/dadwifi-openhal/ath/if_ath.c
+++ branches/dadwifi-openhal/ath/if_ath.c
@@ -8723,7 +8723,7 @@ ATH_SYSCTL_DECL(ath_sysctl_halparam, ctl, write, filp, buffer, lenp, ppos)
 				 * 1 = antenna port 1
 				 * 2 = antenna port 2
 				 */
-				if (val < 0 || val > 2)
+				if (val > 2)
 					return -EINVAL;
 				else
 					sc->sc_txantenna = val;
@@ -8735,7 +8735,7 @@ ATH_SYSCTL_DECL(ath_sysctl_halparam, ctl, write, filp, buffer, lenp, ppos)
 				 * 1 = antenna port 1
 				 * 2 = antenna port 2
 				 */
-				if (val < 0 || val > 2)
+				if (val > 2)
 					return -EINVAL;
 				else
 					ath_setdefantenna(sc, val);
@@ -8745,7 +8745,7 @@ ATH_SYSCTL_DECL(ath_sysctl_halparam, ctl, write, filp, buffer, lenp, ppos)
 				 * 0 = disallow use of diversity
 				 * 1 = allow use of diversity
 				 */
-				if (val < 0 || val > 1)
+				if (val > 1)
 					return -EINVAL;
 				/* Don't enable diversity if XR is enabled */
 				if (((!sc->sc_hasdiversity) || (sc->sc_xrtxq != NULL)) && val)
