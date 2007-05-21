d18308ee93c6b019ecc04d1f21e2f8b4fffb2d29
mentor <mentor@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-05-21 15:55:07 +0000

    ng-refcount has been merged; remove branch

--- branches/madwifi-ng-refcount/scripts/madwifi-unload.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for module in ath_{pci,rate_{amrr,minstrel,onoe,sample},hal} wlan{_{wep,tkip,ccmp,acl,xauth,scan_{sta,ap}},}
-do
-	 grep -q ^$module /proc/modules && modprobe -r $module
-done
