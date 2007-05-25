1d795ff6d9beb247eda69a7b14fe7c88ab0a8d43
mtaylor <mtaylor@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-05-25 22:32:31 +0000

    Do not return error from madwifi-unload.bash if module was not loaded

--- trunk/scripts/madwifi-unload.bash
+++ trunk/scripts/madwifi-unload.bash
@@ -2,5 +2,5 @@
 
 for module in ath_{pci,rate_{amrr,minstrel,onoe,sample},hal} wlan{_{wep,tkip,ccmp,acl,xauth,scan_{sta,ap}},}
 do
-	 grep -q ^$module /proc/modules && modprobe -r $module
+	grep -q ^$module /proc/modules && modprobe -r $module || true
 done
