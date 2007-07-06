20cad25be7aa8572b30116e208ffe55e5c0a4b86
mentor <mentor@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-07-06 21:46:34 +0000

    Enable WDS and use the correct addresses in IBSS/AdHoc
    Signed-Off-By: Georg Lukas <georg@boerde.de>

--- trunk/net80211/ieee80211_output.c
+++ trunk/net80211/ieee80211_output.c
@@ -837,10 +837,6 @@ ieee80211_encap(struct ieee80211_node *ni, struct sk_buff *skb, int *framecnt)
 		hdrsize = sizeof(struct ieee80211_frame);
 
 	switch (vap->iv_opmode) {
-	case IEEE80211_M_IBSS:
-	case IEEE80211_M_AHDEMO:
-		ismulticast = IEEE80211_IS_MULTICAST(eh.ether_dhost);
-		break;
 	case IEEE80211_M_WDS:
 		use4addr = 1;
 		ismulticast = IEEE80211_IS_MULTICAST(ni->ni_macaddr);
@@ -853,6 +849,8 @@ ieee80211_encap(struct ieee80211_node *ni, struct sk_buff *skb, int *framecnt)
 		} else
 			ismulticast = IEEE80211_IS_MULTICAST(eh.ether_dhost);
 		break;
+	case IEEE80211_M_IBSS:
+	case IEEE80211_M_AHDEMO:
 	case IEEE80211_M_STA:
 		if ((vap->iv_flags_ext & IEEE80211_FEXT_WDS) &&
 		    !IEEE80211_ADDR_EQ(eh.ether_shost, vap->iv_myaddr)) {
@@ -869,7 +867,9 @@ ieee80211_encap(struct ieee80211_node *ni, struct sk_buff *skb, int *framecnt)
 					ieee80211_add_wds_addr(nt, ni, eh.ether_shost, 0);
 			}
 		} else
-			ismulticast = IEEE80211_IS_MULTICAST(ni->ni_bssid);
+			ismulticast = (vap->iv_opmode == IEEE80211_M_STA) ?
+					IEEE80211_IS_MULTICAST(ni->ni_bssid) :
+					IEEE80211_IS_MULTICAST(eh.ether_dhost);
 		break;
 	default:
 		break;
