500fbd66c304617a22da74d0b3084561393d85a6
mentor <mentor@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-07-27 16:49:56 +0000

    Move virtual FS setup to a late vattach, as dev.kobj is initialised by register_netdevice in ieee80211_vap_setup

--- trunk/net80211/ieee80211_linux.h
+++ trunk/net80211/ieee80211_linux.h
@@ -539,7 +539,7 @@ static __inline unsigned long msecs_to_jiffies(const unsigned int m)
 	proc_dointvec(ctl, write, filp, buffer, lenp, ppos)
 #endif
 
-void ieee80211_virtfs_vattach(struct ieee80211vap *);
+void ieee80211_virtfs_latevattach(struct ieee80211vap *);
 void ieee80211_virtfs_vdetach(struct ieee80211vap *);
 int ieee80211_proc_vcreate(struct ieee80211vap *, struct file_operations *,
 	       char *);
