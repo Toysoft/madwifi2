cab9c620fba0b57df6fe521eed11297aed89e264
mrenzmann <mrenzmann@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-10-04 12:15:12 +0000

    Remove imported and no longer needed code from sf.net's CVS.

--- cvs-import/branches/WLAN2/net80211/ieee80211-mod.h
+++
@@ -1,5 +0,0 @@
-#ifndef IEEE80211_MOD_H
-#define IEEE80211_MOD_H
-
-
-#endif /* IEEE80211_MOD_H */
--- cvs-import/branches/WLAN2/net80211/linux/if_wireless.h
+++
@@ -1,8 +0,0 @@
-#ifndef IF_WIRELESS_H
-#define IF_WIRELESS_H
-
-struct iw_statistics * ieee80211_iw_getstats(struct net_device *dev);
-
-extern const struct iw_handler_def ieee80211_iw_handler_def;
-
-#endif /* IF_WIRELESS_H */
