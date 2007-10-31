f8cc940cc2c2a3c2d363b57c338e65fc3b557d28
br1 <br1@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2007-10-31 01:05:18 +0000

    allow logging of function names

--- madwifi/branches/madwifi-trace/ath_hal/ah_os.h
+++ madwifi/branches/madwifi-trace/ath_hal/ah_os.h
@@ -228,13 +228,9 @@ extern	u_int32_t __ahdecl ath_hal_reg_read(struct ath_hal *ah, u_int reg);
 
 extern char *ath_hal_func;
 static inline void ath_hal_set_function(const char *name)
-#if defined(AH_DEBUG)
 {
 	ath_hal_func = (char *)name;
 }
-#else
-{ }
-#endif
 
 #ifdef AH_DEBUG_ALQ
 extern	void __ahdecl OS_MARK(struct ath_hal *, u_int id, u_int32_t value);
