832cf4f61864d48082103402d28398c7945ba4ea
mentor <mentor@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2008-01-26 17:51:31 +0000

    Merge trunk to -dfs - r3261

--- madwifi/branches/madwifi-dfs/ath/if_ath.c
+++ madwifi/branches/madwifi-dfs/ath/if_ath.c
@@ -4024,7 +4024,7 @@ ath_key_update_end(struct ieee80211vap *vap)
 	struct ath_softc *sc = dev->priv;
 
 	DPRINTF(sc, ATH_DEBUG_KEYCACHE, "%s:\n", __func__);
-	netif_start_queue(dev);
+	netif_wake_queue(dev);
 	if (!in_softirq())		/* NB: see above */
 		tasklet_enable(&sc->sc_rxtq);
 }
@@ -8412,7 +8412,7 @@ ath_draintxq(struct ath_softc *sc)
 				ath_tx_stopdma(sc, &sc->sc_txq[i]);
 	}
 	sc->sc_dev->trans_start = jiffies;
-	netif_start_queue(sc->sc_dev);		/* XXX move to callers */
+	netif_wake_queue(sc->sc_dev);		/* XXX move to callers */
 	for (i = 0; i < HAL_NUM_TX_QUEUES; i++)
 		if (ATH_TXQ_SETUP(sc, i))
 			ath_tx_draintxq(sc, &sc->sc_txq[i]);
@@ -9177,7 +9177,7 @@ done:
 		ath_grppoll_stop(vap);
 #endif
 bad:
-	netif_start_queue(dev);
+	netif_wake_queue(dev);
 	dev->watchdog_timeo = (sc->sc_dfs_cac ? 120 : 5) * HZ;		/* set the timeout to normal */
 	return error;
 }
@@ -9272,7 +9272,7 @@ ath_dfs_channel_check_completed(unsigned long data )
 #endif
 			}
 		}
-		netif_start_queue(dev);
+		netif_wake_queue(dev);
 		ath_reset(dev);
 		if (sc->sc_beacons) {
 			ath_beacon_config(sc, NULL);
@@ -12710,7 +12710,7 @@ ath_return_txbuf_locked(struct ath_softc *sc, struct ath_buf **bf)
 		++sc->sc_reapcount;
 		if (sc->sc_reapcount > ATH_TXBUF_FREE_THRESHOLD) {
 			if (!ath_radio_silence_required_for_dfs(sc)) {
-				netif_start_queue(sc->sc_dev);
+				netif_wake_queue(sc->sc_dev);
 				DPRINTF(sc, ATH_DEBUG_ANY,
 				    "%s: restarting queue.\n",
 				    __func__);
