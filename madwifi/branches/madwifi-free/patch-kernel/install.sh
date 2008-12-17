ae675601cfc7028acc31a8b2a917a78aacd518dd
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2008-12-17 18:41:06 +0000

    Merge ath_hal into hal directory, remove references to ath_hal
    We don't need to keep a separate wrapper directory anymore.  Also remove
    more references to TARGET.

--- madwifi/branches/madwifi-free/patch-kernel/install.sh
+++ madwifi/branches/madwifi-free/patch-kernel/install.sh
@@ -74,7 +74,7 @@ cp -f ${SRC}/BuildCaps.inc ${SRC}/svnversion.h ${SRC}/release.h ${MADWIFI}
 
 
 echo "Copying source files"
-FILES=$(cd ${SRC} && find ath ath_hal ath_rate hal include net80211 -name '*.[ch]')
+FILES=$(cd ${SRC} && find ath ath_rate hal include net80211 -name '*.[ch]')
 for f in $FILES; do
 	case $f in
 		*.mod.c) continue;;
@@ -88,7 +88,6 @@ FILES=$(cd ${SRC} && find . -name Makefile.kernel)
 for f in $FILES; do
 	cp -f ${SRC}/$f $(dirname ${MADWIFI}/$f)/Makefile
 done
-cp -f ${SRC}/ath_hal/ah_target.inc ${MADWIFI}/ath_hal
 
 echo "Copying Atheros HAL files"
 DST_HAL=${MADWIFI}/hal
