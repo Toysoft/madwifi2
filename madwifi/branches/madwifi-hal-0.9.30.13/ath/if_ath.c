8a3d96082d2f85ae6ab0d013e620c942478de49c
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2009-04-12 05:55:55 +0000

    Avoid triggering lockdep checker in ath_hw_reset()
    Use spin_lock_irqsave_nested() instead of spin_lock_irqsave().  Use a
    separate counter for the locking subclass, so that we don't exceed the
    maximal value of 8.  We have 10 queues, but inactive queues don't get a
    locking subclass number.
    
    Provide a replacement for spin_lock_irqsave_nested() for kernels older
    than 2.6.19.

--- madwifi/branches/madwifi-hal-0.9.30.13/ath/if_ath.c
+++ madwifi/branches/madwifi-hal-0.9.30.13/ath/if_ath.c
@@ -1631,7 +1631,7 @@ static HAL_BOOL ath_hw_reset(struct ath_softc *sc, HAL_OPMODE opmode,
 	HAL_BOOL ret;
 	unsigned long __axq_lockflags[HAL_NUM_TX_QUEUES];
 	struct ath_txq * txq;
-	int i;
+	int i, lclass = 0;
  	u_int8_t old_privFlags = sc->sc_curchan.privFlags;
 
 	/* ath_hal_reset() resets all TXDP pointers, so we need to
@@ -1641,7 +1641,8 @@ static HAL_BOOL ath_hw_reset(struct ath_softc *sc, HAL_OPMODE opmode,
 	for (i = 0; i < HAL_NUM_TX_QUEUES; i++) {
 		if (ATH_TXQ_SETUP(sc, i)) {
 			txq = &sc->sc_txq[i];
-			spin_lock_irqsave(&txq->axq_lock, __axq_lockflags[i]);
+			spin_lock_irqsave_nested(&txq->axq_lock,
+						 __axq_lockflags[i], lclass++);
 		}
 	}
 
