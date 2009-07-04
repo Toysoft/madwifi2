7bc00cca17654d9b346795abe738277d1704e8b6
proski <proski@fb07743e-f919-4851-9fd0-ac59733bbbc5>
2009-07-04 03:10:46 +0000

    Merge r4022, r4059 and r4061 from trunk

--- madwifi/branches/madwifi-hal-0.10.5.6/ath_hal/ah_os.h
+++ madwifi/branches/madwifi-hal-0.10.5.6/ath_hal/ah_os.h
@@ -198,8 +198,9 @@ extern void *__ahdecl ath_hal_memcpy(void *, const void *, size_t);
 			   (0x7000 <= (__reg) && (__reg) < 0x8000))
 # if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,12)
 #  define _OS_REG_WRITE(_ah, _reg, _val) do {			\
-	 is_reg_le(_reg) ?					\
-	  iowrite32((_val), (_ah)->ah_sh + (_reg)) :		\
+	 if (is_reg_le(_reg))					\
+	  iowrite32((_val), (_ah)->ah_sh + (_reg));		\
+	 else							\
 	  iowrite32be((_val), (_ah)->ah_sh + (_reg));		\
 	} while (0)
 #  define _OS_REG_READ(_ah, _reg)				\
